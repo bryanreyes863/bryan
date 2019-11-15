c52bf3a105f7cc5a66d64d469c665185a0cb7247
bryanreyes863 <bryanreyes863@gmail.com>
2019-11-15 15:19:32 +0800

    fixing animation

--- static/main.js
+++ static/main.js
@@ -527,7 +527,6 @@ loader2.loadManifest(maniFest2,true);
 
 window.addEventListener("resize" , resize);
 
-
 function handleProgress() {
 	total = Math.round(loader.progress / 1 * 100);
 
@@ -1050,83 +1049,83 @@ function runTrack(num,num2){
 					finish = true;
 				},2500)
 
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_49.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+49));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// setTimeout(function(){
+				// 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_49.png");
+				// 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+49));
+				// 	bgContainer.removeAllChildren();
+				// 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},80)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_50.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+50));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// 	var windowW = window.innerWidth;
+				// 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// 	stage.update();
+				// },80)
+				// setTimeout(function(){
+				// 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_50.png");
+				// 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+50));
+				// 	bgContainer.removeAllChildren();
+				// 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},160)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_51.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+51));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// 	var windowW = window.innerWidth;
+				// 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// 	stage.update();
+				// },160)
+				// setTimeout(function(){
+				// 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_51.png");
+				// 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+51));
+				// 	bgContainer.removeAllChildren();
+				// 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},240)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_52.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+52));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// 	var windowW = window.innerWidth;
+				// 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// 	stage.update();
+				// },240)
+				// // setTimeout(function(){
+				// // 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_52.png");
+				// // 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+52));
+				// // 	bgContainer.removeAllChildren();
+				// // 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},320)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_53.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+53));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// // 	var windowW = window.innerWidth;
+				// // 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	stage.update();
+				// // },320)
+				// // setTimeout(function(){
+				// // 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_53.png");
+				// // 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+53));
+				// // 	bgContainer.removeAllChildren();
+				// // 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},400)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_54.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+54));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// // 	var windowW = window.innerWidth;
+				// // 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	stage.update();
+				// // },400)
+				// // setTimeout(function(){
+				// // 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_54.png");
+				// // 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+54));
+				// // 	bgContainer.removeAllChildren();
+				// // 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},560)
-				setTimeout(function(){
-					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_55.png");
-					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+55));
-					bgContainer.removeAllChildren();
-					bgContainer.addChild(imageBackground);
+				// // 	var windowW = window.innerWidth;
+				// // 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	stage.update();
+				// // },400)
+				// // setTimeout(function(){
+				// // 	$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_55.png");
+				// // 	imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+55));
+				// // 	bgContainer.removeAllChildren();
+				// // 	bgContainer.addChild(imageBackground);
 
-					var windowW = window.innerWidth;
-					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
-					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
-					stage.update();
-				},640)
+				// // 	var windowW = window.innerWidth;
+				// // 	imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
+				// // 	stage.update();
+				// // },400)
 				setTimeout(function(){
 					$('#track_bg').attr("src" , "../assets/images/tinyfier/bg_track_56.png");
 					imageBackground = new createjs.Bitmap(loader2.getResult("bgTrack_"+56));
@@ -1137,7 +1136,7 @@ function runTrack(num,num2){
 					imageBackground.scaleX = (windowW - (windowW / 5)) / bg.image.height;
 					imageBackground.scaleY = (windowW - (windowW / 5)) / bg.image.height;
 					stage.update();
-				},720)
+				},80)
 					
 				
 				$('#track_bg').css('opacity' , 1);
@@ -1145,6 +1144,13 @@ function runTrack(num,num2){
 			}
 	}
 }
+
+
+// window.mobilecheck = function() {
+//   var check = false;
+//   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+//   return check;
+// };
 
 	function changeTrack(num,num2) {
 		// console.log(num)
