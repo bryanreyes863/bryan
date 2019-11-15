e16c7126db4d40ddeba55474f7d052a8382ecd83
bryanreyes863 <bryanreyes863@gmail.com>
2019-11-15 16:21:12 +0800

    countext change

--- static/main.js
+++ static/main.js
@@ -1039,7 +1039,7 @@ function runTrack(num,num2){
 
 			if (window.mobilecheck()) {
 
-				if (num < 406 && count_down.text != '00:27') {
+				if (num < 406 && count_down.text != '00:29') {
 
 						num++;
 						num2++;
