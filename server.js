8956307be4c8cc055660f7d7493555fd210c153f
bryanreyes863 <bryanreyes863@gmail.com>
2019-12-21 15:16:11 +0800

    admin reset

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ server.listen(server_port , server_ip_address , function(){
 	console.log('Listening on' + server_ip_address + ', port' + server_port);	
 })
 
+
 app.use(express.static('./'));
 
 //var section
